39a02ed2b686ca74c460a700b5ebc4f6793fdba0
ArturasVBurneyka <arturburneika@mail.ru>
2023-11-15 14:24:53 -0500

    task-01/terraform/variables.tf was added

---
+++ task-01/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "arturas_oauth_token" {
+    type = string
+}
+
+variable "arturas_cloud_id" {
+    type = string
+}
+
+variable "arturas_folder_id" {
+    type = string
+}
+
+variable "arturas_zone" {
+    type = string
+}
+
+variable "arturas_subnet" {
+    type = string
+}
+
+variable "arturas_user" {
+    type = string
+}
+
+variable "arturas_ssh_public_key" {
+    type = string
+}
