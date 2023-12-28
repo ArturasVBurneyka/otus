d3e30cacb0bc8fa0ff352042f164563fce5b323d
ArturasVBurneyka <arturburneika@mail.ru>
2023-12-28 14:56:59 -0500

    task-03 was added

---
+++ task-03/terraform/variables.tf
@@ -0,0 +1,62 @@
+variable "arturas_oauth_token" {
+    type = string
+}
+
+variable "arturas_cloud_id" {
+    type = string
+}
+
+variable "arturas_folder_id" {
+    type = string
+}
+
+variable "arturas_zone" {
+    type = string
+}
+
+variable "arturas_subnet" {
+    type = string
+}
+
+variable "arturas_user" {
+    type = string
+}
+
+variable "arturas_ssh_public_key" {
+    type = string
+}
+
+variable "arturas_servers" {
+    type = list(object({
+        name        = string
+        cores       = number
+        fractions   = number
+        memory      = number
+    }))
+    default = [
+        {
+            name        = "loadbalancer"
+            cores       = 2
+            fractions   = 5
+            memory      = 1
+        },
+        {
+            name        = "backend-first"
+            cores       = 2
+            fractions   = 5
+            memory      = 1
+        },
+        {
+            name        = "backend-second"
+            cores       = 2
+            fractions   = 5
+            memory      = 1
+        },
+        {
+            name        = "database"
+            cores       = 2
+            fractions   = 5
+            memory      = 1
+        }
+    ]
+}
